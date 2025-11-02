6b259495c0479a6f4577d5a89a6ddd535294c0aa
myeongsang <55619928+cvb12345@users.noreply.github.com>
2025-11-03 01:22:17 +0900

    three

---
+++ C_Language/1014.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char c, c1;
+    scanf("%c %c", &c, &c1);
+    printf("%c %c", c1, c);
+    return 0;
+}
---
+++ C_Language/1023.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int d, d1;
+    scanf("%d.%d", &d, &d1);
+    printf("%d\n%d", d, d1);
+    return 0;
+}
